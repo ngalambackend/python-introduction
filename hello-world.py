4d35da2fdfb83dc5cb3b072446acad27873cedbc
abdullahaaf <abdullahaaf1@gmail.com>
2019-09-20 10:36:35 +0700

    initial commit

---
+++ hello-world.py
@@ -0,0 +1,21 @@
+# simple print statement in python
+# print("Hello World !")
+
+
+'''
+INDENTATION
+standart indentation use 4 spaces
+'''
+
+x = 4
+if x==1:
+    print("Hello World")
+else:
+    print("Wrong Program")
+
+
+
+'''
+EXERCISE
+use the "print" command to print hello world program
+'''
